2b484b28cd791a4bf8366377f31e0022278d2f2b
KesavamurthyT <kesavamurthy1008@gmail.com>
2025-03-29 16:36:06 +0530

    FrontEnd_Modules

---
+++ fundmyfuture-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to FundMyFuture 🎉</h1>
+      <p>Empowering financial independence for the future.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
